acfa4e2a76f5403def30fc0fddaa52f7be02bcef
Mahdi Hassai <hassanim430@gmail.com>
2023-08-11 21:58:56 +0430

    adding header and footer to out frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+
 const App = () => {
-	return <h1>My React App</h1>
-}
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>My React App</h1>
+        </Container>
+      </main>
+	  <Footer />
+    </>
+  );
+};
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link>
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+              <Nav.Link>
+                <FaUser /> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+export default Header;
