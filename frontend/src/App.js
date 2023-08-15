295b5b0dedf6284f32ee63ec1813dff9827af707
Mahdi Hassai <hassanim430@gmail.com>
2023-08-15 21:07:54 +0430

    adding product lists on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>My React App</h1>
+          <HomeScreen />
         </Container>
       </main>
 	  <Footer />
---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export default function Product({ product }) {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
+
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
