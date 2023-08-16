90c57ce2912f0562a10ca75ffcd53339f5f64557
Mahdi Hassai <hassanim430@gmail.com>
2023-08-16 20:48:42 +0430

    adding rating components to frontend

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
 export default function Product({ product }) {
   return (
@@ -15,6 +16,14 @@ export default function Product({ product }) {
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as='div'>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+        
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
