381566e806e3087a95f16b1c2ec6d971771d77ec
Mahdi Hassai <hassanim430@gmail.com>
2023-08-21 18:47:14 +0430

    redux setup and api slice creation

---
+++ frontend/src/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "http://localhost:5000";
+export const PRODUCTS_URL = "/api/products";
+export const USERS_URL = "/api/users";
+export const ORDERS_URL = "/api/orders";
+export const PAYPAL_URL = "/api/config/paypal";
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,20 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/index.css";
 import App from "./App";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import store from "./store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App></App>}>
-        <Route path="/" index={true} element={<HomeScreen></HomeScreen>}></Route>
-        <Route path="/product/:id" element={<ProductScreen></ProductScreen>} ></Route>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App></App>}>
+          <Route path="/" index={true} element={<HomeScreen></HomeScreen>}></Route>
+          <Route path="/product/:id" element={<ProductScreen></ProductScreen>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
